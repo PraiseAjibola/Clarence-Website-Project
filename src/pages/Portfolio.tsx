5b5cc296bbca89924ae290fe165356d167458a19
Praise Oluwadamilola <prayzajibola@gmail.com>
2025-11-02 21:37:42 +0100

    Fix SPA routing for Vercel

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -124,7 +124,7 @@ const Portfolio: React.FC = () => {
 
       {/* Hero Section */}
       <section className="text-center px-[8%]  pb-16">
-        <h1 className="text-[60px] font-bold leading-tight mb-6 md:text-[57px]">Our Investment Portfolio</h1>
+        <h1 className="lg:text-[60px] text-[50px] font-bold leading-tight mb-6 md:text-[57px]">Our Investment Portfolio</h1>
         <p className="text-[21px] text-white/80 max-w-3xl mx-auto mb-12 md:text-[18px]">
           A diversified collection of exceptional businesses driving growth and innovation across Africa's most promising sectors.
         </p>
